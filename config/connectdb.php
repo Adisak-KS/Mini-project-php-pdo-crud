b38536ed39904b41864e73be7dae085e59d36a70
Adisak-KS <Adisak.General@gmail.com>
2024-01-13 10:18:01 +0700

    Create File Connect Database

---
+++ config/connectdb.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "123";
+$dbname = "php_crud";
+
+$dsn = "mysql:host=$servername;dbname=$dbname";
+
+try {
+  $conn = new PDO($dsn, $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+// เก็บ session 
+session_start();
+?>
