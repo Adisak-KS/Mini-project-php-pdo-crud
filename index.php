7adebad6795ed4f81dddeaf950c5c1eaebb20005
Adisak-KS <Adisak.General@gmail.com>
2024-01-13 10:58:53 +0700

    Add Script Preview Profile

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                             <label for="Profile" class="col-form-label">Profile : </label>
                             <input type="file" class="form-control" name="profile" id="profileInput" require>
                             <br>
-                            <img id="previewProfile" alt="">
+                            <img width="100%" id="previewProfile" alt="">
                         </div>
 
                         <div class="modal-footer">
@@ -64,6 +64,24 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <!-- Preview รูปภาพก่อนการ Upload ลง Database -->
+    <script>
+        let profileInput = document.getElementById('profileInput');
+        let previewProfile = document.getElementById('previewProfile');
+
+        profileInput.onchange = evt => {
+            const [file] = profileInput.files;
+            // ถ้ามีการเพิ่มรูปเข้ามา
+            if(file){
+                // ให้แสดงรูปภาพ
+                previewProfile.src = URL.createObjectURL(file);
+            }
+        }
+    </script>
+
+
+
 </body>
 
 </html>
